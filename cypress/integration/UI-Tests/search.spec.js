f30dbfcc5546a35093574cbcb6733c05d6ad714c
Sam Shirley <samuelashirley@gmail.com>
2019-10-23 15:28:16 -0600

    Refactor for reliability

--- cypress/integration/UI-Tests/search.spec.js
+++ cypress/integration/UI-Tests/search.spec.js
@@ -8,20 +8,22 @@ context('Search', () => {
     beforeEach(() => {
         cy.visit('https://www.rocketmiles.com/')
         cy.setCookie('EMAIL_SIGNUP', 'true')
-        
+        cy.wait(2000)
     })
 
-    it('enters New York into location input', () => {
-        cy.get('.location-search-container > .rm-input-base').click()
-        cy.wait(500)
+    it('Fills out search with New York and Amazon Rewards', () => {
         cy.get('.location-search-container > .rm-input-base')
             .click()
+            .wait(1000)
             .type('{downarrow}')
-            .wait(200)
+            .wait(500)
             .type('{downarrow}')
-            .wait(200)
+            .wait(500)
             .type('{enter}')
-        
-        cy.get('.rm-btn-orange > .ng-scope').click()
+        cy.get('.program-autosuggest-container > .rm-input-base')
+            .click()
+            .type('{downarrow}')
+            .type('{enter}')
+            cy.get('.rm-btn-orange > .ng-scope').click()
     })
 })
